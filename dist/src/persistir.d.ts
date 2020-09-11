d50addc99673f3393735f59a59d4dfc7e052486a
demervalgomes <demerval@supremustecnologia.com.br>
2020-09-11 19:59:44 -0300

    Esqueci do build

--- dist/src/persistir.d.ts
+++ dist/src/persistir.d.ts
@@ -2,12 +2,18 @@ import { Status } from './enums';
 import { ItemConsulta } from './consulta';
 export interface ConfigPersist {
     persistir: ItemPersist[];
+    persistirSql?: ItemPersistSql[];
     consultar?: ItemPersitConsulta[];
 }
 export interface ItemPersist {
     id: string;
     status: Status;
     dados: any;
+}
+export interface ItemPersistSql {
+    id: string;
+    retornar: boolean;
+    sql: string;
 }
 export interface ItemPersitConsulta extends ItemConsulta {
     idConsulta?: {
